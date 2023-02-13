6431642a6253974ad9d9a147c835f1015ae54445
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2023-02-13 19:33:25 +0100

    Comment passport

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -22,39 +22,39 @@ passport.deserializeUser(function (user, cb) {
 });
 
 // FOR GOOGLE STRATEGY FROM PASSPORT
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: 'http://localhost:8080/auth/google/secrets',
-      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile, 'google');
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
-    }
-  )
-);
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.CLIENT_ID,
+//       clientSecret: process.env.CLIENT_SECRET,
+//       callbackURL: 'http://localhost:8080/auth/google/secrets',
+//       userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//       console.log(profile, 'google');
+//       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//         return cb(err, user);
+//       });
+//     }
+//   )
+// );
 
 //FOR FACEBOOK STRATEGY FROM PASSPORT
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: 'http://localhost:8080/auth/facebook/secret',
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile, 'facebook');
-      User.findOrCreate(
-        { facebookId: profile.id, email: profile.email },
-        function (err, user) {
-          return cb(err, user);
-        }
-      );
-    }
-  )
-);
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: process.env.FACEBOOK_APP_ID,
+//       clientSecret: process.env.FACEBOOK_APP_SECRET,
+//       callbackURL: 'http://localhost:8080/auth/facebook/secret',
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//       console.log(profile, 'facebook');
+//       User.findOrCreate(
+//         { facebookId: profile.id, email: profile.email },
+//         function (err, user) {
+//           return cb(err, user);
+//         }
+//       );
+//     }
+//   )
+// );
